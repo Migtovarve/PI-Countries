165cec226d2fa1f2e6fe5a2feff48f9d74f33ddb
unknown <Migtovarve@gmail.com>
2022-06-15 20:30:30 -0400

    responsive form

--- api/src/db.js
+++ api/src/db.js
@@ -7,7 +7,7 @@ const {
 } = process.env;
 
 /*
-*/
+
 let sequelize =
   process.env.NODE_ENV === "production"
     ? new Sequelize({
@@ -36,13 +36,13 @@ let sequelize =
         `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/countries`,
         { logging: false, native: false }
       );
-/*
+
 */
 
-// const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/countries`, {
-//   logging: false, // set to console.log to see the raw SQL queries
-//   native: false, // lets Sequelize know we can use pg-native for ~30% more speed
-// });
+const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/countries`, {
+  logging: false, // set to console.log to see the raw SQL queries
+  native: false, // lets Sequelize know we can use pg-native for ~30% more speed
+});
 const basename = path.basename(__filename);
 
 const modelDefiners = [];
--- client/src/components/Card/Card.js
+++ client/src/components/Card/Card.js
@@ -6,10 +6,11 @@ export default function Card({name, continent, flag, id}) {
     return (
         <div className={style.content}>
             <Link to={`/countries/${id}`}><img src={flag} alt="img not found"/></Link>
+            <div>
             <Link to={`/countries/${id}`}><h2 className={style.title}>{(name[0].toUpperCase() + name.slice(1))}</h2></Link>
             <h3 className={style.subTitle}>{continent}</h3>
+            </div>
         </div>
-
     )
 }
 
